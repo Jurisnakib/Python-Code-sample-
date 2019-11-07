b096e3a305060dad69181dd4840dfc3cb7e1fa22
JurisNakib <nakib_juris@outlook.com>
2019-11-07 22:13:02 +0600

    Lower case we can declare

--- CarGame.py
+++ CarGame.py
@@ -1,8 +1,12 @@
 command = ""
 
 while command.lower() != "quit":
-    command =input("> ")
-    if command.lower() == "start":
+
+    #Lower case we can declare here so that we dont need to write all the time. 
+    command =input("> ").lower()
+    if command == "start":
         print ("Car started....")
+    elif command == "stop":
+        print("Car stopped.")
         
     pass

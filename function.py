0eee776e9388c9d53e6abd8308d0c5227dad5f35
M A Nakib <nakib_juris@outlook.com>
2020-05-11 15:05:55 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,8 @@
+def testFunction():
+    print ("Test Function")
+    x, y = 4, 6
+    z = x + y
+    print (z)
+testFunction()
+testFunction()
+
---
+++ test.py
@@ -0,0 +1,18 @@
+d = 2
+f = 4
+g = d+f
+h = [1,2,3,4,5,6,7,8,9,10]
+print (g)
+
+if g >= 8:
+    print (20)
+else:
+    print ("10")
+
+while d == 2:
+    
+    d = d+1
+    print (d)
+for i in h:
+    print (i)
+    

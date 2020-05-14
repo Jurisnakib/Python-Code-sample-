760089726f57cd6557fa4011558ed551f75dbb46
M. A. Nakib (Juris) <nakib_juris@outlook.com>
2020-05-14 14:12:42 +0600

    global veriable and local veriable

--- function.py
+++ function.py
@@ -4,5 +4,23 @@ def testFunction():
     z = x + y
     print (z)
 testFunction()
-testFunction()
+#testFunction()
+# create 2 fuction and do sum
+
+def sum(a, b):
+    sum = a + b
+    print (sum)
+
+sum (9,10)
+sum (1,2)
+
+#Paramiter with veriable.
+
+def WishList(Name, Wish = "Happy Birthday to you"):
+    print(Wish,Name)
+
+WishList ("Nakib")
+
+#use global and local veriable.
+
 

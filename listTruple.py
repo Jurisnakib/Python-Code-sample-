91852d032e3a4f954f5c51e7d5ccea62e81f1aa6
M. A. Nakib (Juris) <nakib_juris@outlook.com>
2020-05-23 01:08:30 +0600

    insert list with multiple number and otheres veriable

---
+++ listTruple.py
@@ -0,0 +1,11 @@
+list = [2, 44, 33, 4, 55, 677,1, 54, 2, 2]
+truple = (1,2,3,4,5,6,7,8,12,2,45,46,57,67)
+list [0] = 1
+
+print(list[0])
+print(truple[1])
+#insert multimple number to list
+
+list = [[2,2],43,23,5,4,65,7,5,6,7,453, "Hello", "Nakib"]
+
+print ( list)

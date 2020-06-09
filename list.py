b5b0470c77110f7dfb442a72a600b47123ab254b
M. A. Nakib (Juris) <nakib_juris@outlook.com>
2020-06-09 16:18:53 +0600

    #Data insert to your list on python

--- list.py
+++ list.py
@@ -1,7 +1,7 @@
 #Multidymentional list 
 
-list = [[0,2,222],1,2,34,5,6,7,824,22]
-print (list[0][1])
+# list = [[0,2,222],1,2,34,5,6,7,824,22]
+# print (list[0][1])
 
 #delet duplicate number 
 # numbers = [1, 8, 7, 5, 3, 10, 8, 3]
@@ -13,5 +13,10 @@ print (list[0][1])
 # print(unique)
     
 
+#Data insert to your list on python 
+list = [1,2,3,4,5,6,7,8,835,32,124,]
 
+list.insert(5, 420)
+
+print (list)
 

72e5fa1a5cb5865f2a819c8a020cc176a4b87cc9
M. A. Nakib (Juris) <nakib_juris@outlook.com>
2020-06-09 14:52:04 +0600

    #Multidymentional list on python

--- list.py
+++ list.py
@@ -1,11 +1,16 @@
-#delet duplicate number 
-numbers = [1, 8, 7, 5, 3, 10, 8, 3]
-unique = []
+#Multidymentional list 
 
-for number in numbers:
-    if number not in unique:
-        unique.append(number)
-print(unique)
+list = [[0,2,222],1,2,34,5,6,7,824,22]
+print (list[0][1])
+
+#delet duplicate number 
+# numbers = [1, 8, 7, 5, 3, 10, 8, 3]
+# unique = []
+
+# for number in numbers:
+#     if number not in unique:
+#         unique.append(number)
+# print(unique)
     
 
 
